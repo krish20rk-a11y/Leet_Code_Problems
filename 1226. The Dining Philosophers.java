0be55f97ede97483f5fab0bd9dcef7a35e322f61
krish20rk-a11y <krish20rk@gmail.com>
2026-07-28 09:06:50 +0530

    Create 1226. The Dining Philosophers.java

---
+++ 1226. The Dining Philosophers.java
@@ -0,0 +1,39 @@
+class DiningPhilosophers {
+
+    private final ReentrantLock[] forks;
+
+    public DiningPhilosophers() {
+        forks = new ReentrantLock[5];
+        for (int i = 0; i < 5; i++) {
+            forks[i] = new ReentrantLock();
+        }
+    }
+
+    public void wantsToEat(int philosopher,
+                           Runnable pickLeftFork,
+                           Runnable pickRightFork,
+                           Runnable eat,
+                           Runnable putLeftFork,
+                           Runnable putRightFork) throws InterruptedException {
+
+        int left = philosopher;
+        int right = (philosopher + 1) % 5;
+
+        if (philosopher % 2 == 0) {
+            forks[left].lock();
+            forks[right].lock();
+        } else {
+            forks[right].lock();
+            forks[left].lock();
+        }
+
+        pickLeftFork.run();
+        pickRightFork.run();
+        eat.run();
+        putRightFork.run();
+        putLeftFork.run();
+
+        forks[right].unlock();
+        forks[left].unlock();
+    }
+}
